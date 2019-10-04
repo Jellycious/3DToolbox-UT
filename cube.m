99ecff71ad13641e65741b5ca75d24061b095e26
Jelle Meijer <jellejanwil@gmail.com>
2019-10-04 13:26:28 +0200

    jelle branch

---
+++ cube.m
@@ -0,0 +1,7 @@
+classdef cube       %Unit cube
+    properties 
+        Vertices    %Points
+        Edges       %Edges between points
+    end
+    
+end
---
+++ jsquare.m
@@ -0,0 +1,6 @@
+classdef jsquare 
+    properties
+        Vertices = [0 0 0;0 1 0;0 1 1;0 0 1];
+        Edges = [0 1 2 3];
+    end
+end
