c68810ff99235a0cb963c0da4242dd8d51210112
Jelle Meijer <jellejanwil@gmail.com>
2019-10-10 19:42:39 +0200

    cleanup

--- cube.m
+++
@@ -1,7 +0,0 @@
-classdef cube       %Unit cube
-    properties 
-        Vertices    %Points
-        Edges       %Edges between points
-    end
-    
-end
--- jsquare.m
+++
@@ -1,6 +0,0 @@
-classdef jsquare 
-    properties
-        Vertices = [0 0 0;0 1 0;0 1 1;0 0 1];
-        Edges = [0 1 2 3];
-    end
-end
