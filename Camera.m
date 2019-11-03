500d5d437697685cc11931a1e53208cce2d8de8f
Marten <me@marten.xyz>
2019-11-03 14:57:38 +0100

    Line projection werkt deels (iig voor lijnen vanuit de origin (assen))

--- Camera.m
+++ Camera.m
@@ -2,6 +2,7 @@ classdef Camera
     properties
         E
         T
+        C
         d
         alpha
         screensize
@@ -31,6 +32,9 @@ classdef Camera
             self.E = E0;
             self.T = T0;
             
+            t_n = (self.T-self.E)/norm(self.T-self.E);
+            self.C = self.d * t_n + self.E;
+            
             self.screensize = self.d * tan(self.alpha/2);
         end
         
@@ -51,7 +55,7 @@ classdef Camera
             % Remove all points behind camera
             % TODO: Do some better checks, and move points to screen edge
             % if required.
-            a(a < 0) = 0;
+%             a(a < 0) = 0;
 
             % Calculate projected points
             pj = a .* r + self.E;
@@ -79,33 +83,12 @@ classdef Camera
         end
         
         function drawAxis(self)
-            I_x = [1 0];
-            I_y = [0 1];
-
             % The x,y,z normal vectors
-            p = [1 0 0; 0 1 0; 0 0 1];
-            % Distance from origin to screen side
-            self.screensize = self.d * tan(self.alpha/2);
-
-            % Project normal vectors
-            q = self.project(p);
-            
-            % TODO: Ths part can probably be done more cleanly
-            a_x = self.screensize ./ (q*I_x');
-            a_y = self.screensize ./ (q*I_y');
-            a = min(abs(a_x), abs(a_y));        % Find min scalar needed for 
-                                                % intersection with screen side
-
-            % Convert resulting scalar into two points and format them like:
-            % | x00 y00 |
-            % | x01 y01 |
-            % | x10 y10 |
-            % | x11 y11 |
-            rows = size(p, 1)*2;
-            pt = zeros(rows, 2);
-
-            pt(1:2:rows, :) = -a.*q;
-            pt(2:2:rows, :) = a.*q;
+            p = [1 0 0; 0 0 0;
+                 0 1 0; 0 0 0;
+                 0 0 1; 0 0 0];
+             
+            pt = self.projectLine(p);
             
             self.drawToScreen(pt, 'black');
         end
@@ -115,7 +98,7 @@ classdef Camera
             self.drawToScreen(q, color);
         end
     
-        function projectAndDrawAnaglyph(self, p)
+        function self = projectAndDrawAnaglyph(self, p)
             E0 = self.E;
             
             %% Draw left eye
@@ -130,6 +113,50 @@ classdef Camera
             
             %% Reset
             self.E = E0;
+        end
+        
+        % @param p: matrix with [p; p0; q; q0;] etc.
+        function pt = projectLine(self, p)
+            if mod(p,2) ~= 0
+                error('Dimensions of dir and offset need to be equal');
+            end
+            
+            % Just some tools. Normal vectors of xy plane
+            I_x = [1 0];
+            I_y = [0 1];
+
+            % Project normal vectors
+            q = self.project(p);
+            q0 = q(2:2:end, :);     % Offset
+            q = q(1:2:end, :);      % Direction vector
+            
+            % TODO: Ths part can probably be done more cleanly
+            v = q - q0;
+%             a_xr = (self.screensize - q0*I_x') ./ (v*I_x')       % v*I_x' == dotproduct on every row
+%             a_xl = (-self.screensize - q0*I_x') ./ (v*I_x')
+%             a_yt = (self.screensize - q0*I_y') ./ (v*I_y')
+%             a_yb = (-self.screensize - q0*I_y') ./ (v*I_y')
+            
+            a_x = (self.screensize*[1, -1] - q0*I_x') ./ (v*I_x');
+            a_y = (self.screensize*[1, -1] - q0*I_y') ./ (v*I_y');
+            
+            aa = [a_x; a_y]
+            a(:,1) = min(aa(aa>0));
+            a(:,2) = max(aa(aa<=0));
+%             a = min(abs(a_x), abs(a_y));        % Find min scalar needed for
+                                                % intersection with screen edge
+
+                                                
+            % Convert resulting scalar into two points and format them like:
+            % | x00 y00 |
+            % | x01 y01 |
+            % | x10 y10 |
+            % | x11 y11 |
+            rows = size(v, 1)*2;
+            pt = zeros(rows, 2);
+
+            pt(1:2:rows, :) = a(:,1).*v + q0;
+            pt(2:2:rows, :) = a(:,2).*v + q0
         end
     end
 end
